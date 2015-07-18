3e4f7af94d9042fa11e6db8559b76c9d9cb48976
BrianWeinstein <bweinstein02@gmail.com>
2015-07-18 19:28:06 -0400

    most of problem 1, still some projection issues

--- homework_2/hw02_q1.R
+++ homework_2/hw02_q1.R
@@ -17,6 +17,7 @@
 setwd("~/Documents/data-mining/homework_2")
 
 # load libraries
+library(ggplot2)
 
 
 #################
@@ -41,6 +42,9 @@ rawData.rowmeans <- apply(rawData, 1, mean)
 # center the data
 data <- as.matrix(apply(rawData, 2, function(col) col - mean(col)))
 
+# standardize the data
+#data <- apply(data, 2, function(col) col/sd(col))
+
 # empirical covariance matrix
 sig <- (1/nrow(data))*t(data)%*%data
 
@@ -59,22 +63,87 @@ evecs <- eigen(sig)$vectors
 #################
 # Problem 1d
 #################
-#d. (5 Points) Give all of the loadings and all of the scores for the data.
 
 # loadings
 evecs
 
 # scores
-data%*%evecs
+data%*%t(evecs)
 
 
-# 
-# e. (5 Points) Plot the proportion of variance captured against the number of components included. How many components should we include and why?
-# f. (5 Points) Load hw02 q1 p2.csv. This has 5 new observations in the original coordinates. Give their scores.
-# g. (5 Points) Now use only the first two scores to represent the observations from the previous part. What are the coordinates of the projections in the original space, x′? What is their Euclidean distance from the original data points?
-# h. (5 Points) Define the error of a point as
-# d(x′, x) = x′ − x,
-# which is a 5-dimensional vector of errors. In what direction is d(x′,x) for the 5 new points? Why do you think this is?
-# 
-# 
-# 
+
+#################
+# Problem 1e
+#################
+
+pve <- data.frame(var=evals/sum(evals))
+
+ggplot(pve, aes(x=1:ncol(sig), y=var)) +
+  geom_line() + geom_point() +
+  xlab("principal component #") + ylab("pve") + theme_bw()
+ggsave(filename='writeup/1e.png', width=5, height=3)
+
+
+
+#################
+# Problem 1f
+#################
+
+# read in hw02_q1_p2.csv
+rawData2 <- as.matrix(read.csv(file="datasets/hw02_q1_p2.csv"))
+
+# column means from part 1a
+rawData.colmeans 
+
+# center the data
+data2 <- t(t(rawData2)-rawData.colmeans)
+
+# empirical covariance matrix
+sig2 <- (1/nrow(data2))*t(data2)%*%data2
+
+# eigenvalues and eigenvectors
+evals2 <- eigen(sig2)$values
+evecs2 <- eigen(sig2)$vectors
+
+# loadings
+evecs2
+
+# scores
+scores2 <- data2%*%t(evecs2)
+
+
+#################
+# Problem 1g
+#################
+
+# use only the first 2 scores
+scores2_2d <- scores2
+scores2_2d[, 3:5] <- 0
+
+# coordinates of the projections in the original space, x'
+projInOrigSpace <- scores2_2d%*%evecs2
+projInOrigSpace <- t(t(projInOrigSpace)+rawData.colmeans) # uncenter the data
+
+
+# define a distance function
+euc.dist <- function(vec1, vec2){
+  sqrt(sum((vec1-vec2)^2))
+}
+
+# euclidean distance from the original data points
+for(obs in 1:nrow(data2)){
+  dist <- euc.dist(projInOrigSpace[obs, ], rawData2[obs, ])
+  print(dist)
+}
+
+
+
+#################
+# Problem 1h
+#################
+
+(projInOrigSpace-rawData2)
+
+plot((projInOrigSpace-rawData2)[,1],(projInOrigSpace-rawData2)[,2])
+arrows(0,0, 20*evecs2[1,1],20*evecs2[2,1],lwd=3,col="red")
+
