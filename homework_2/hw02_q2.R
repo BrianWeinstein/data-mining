e037156e282d3642b28e4eb768a9ae91c537e2db
BrianWeinstein <bweinstein02@gmail.com>
2015-07-19 01:11:10 -0400

    problem 2b-2e

--- homework_2/hw02_q2.R
+++ homework_2/hw02_q2.R
@@ -19,13 +19,11 @@ setwd("~/Documents/data-mining/homework_2")
 # load libraries
 library(pixmap)
 library(data.table)
+library(ggplot2)
 
 # the list of pictures (note the absence of 14 means that 31 corresponds to yaleB32)
 pic_list <- 1:38
 view_list <- c('P00A+000E+00', 'P00A+005E+10', 'P00A+005E-10', 'P00A+010E+00')
-
-
-
 
 
 
@@ -68,12 +66,12 @@ faces_matrix <- vector()
 # Convert each pgm file to a row of data
 faces_matrix <- lapply(pic_data,
                        function(img){
-                         as.vector(t(getChannels(img)), mode="list")
+                         as.vector(t(getChannels(img)), mode="any")
                        }
 )
 
 # row bind each vector (i.e., each photo) into one matrix
-faces_matrix <- do.call(rbind, faces_matrix)
+faces_matrix <- as.matrix(do.call(rbind, faces_matrix))
 
 # check size of matrix
 dim(faces_matrix)
@@ -89,6 +87,13 @@ dim(faces_matrix)
 
 #----- START YOUR CODE BLOCK HERE -----#
 
+# mean face
+mean_face <- apply(faces_matrix, 2, mean)
+mean_face <- matrix(mean_face, 192, 168, byrow=TRUE)
+plot(pixmapGrey(mean_face))
+dev.copy(device=png, file='writeup/2b_mean_face.png', height=192, width=168)
+#dev.copy(device=png, file='writeup/2b_mean_face.png', height=400, width=400*(168/192))
+dev.off()
 
 #----- END YOUR CODE BLOCK HERE -----#
 
@@ -100,6 +105,21 @@ dim(faces_matrix)
 
 #----- START YOUR CODE BLOCK HERE -----#
 
+# column center faces_matrix
+faces_centered <- apply(faces_matrix, 2, function(col){col - mean(col)})
+#faces_centered <- scale((faces_matrix), center=TRUE, scale=FALSE)
+
+# pca on the centered data
+pc <- prcomp(faces_centered)
+
+# calculate pve
+pc.pve <- data.frame(pve=((pc$sdev)^2)/sum((pc$sdev)^2))
+
+# plot pve
+ggplot(pc.pve, aes(x=1:nrow(pc.pve), y=pve)) +
+  geom_line() + geom_point() +
+  xlab("principal component #") + ylab("pve") + theme_bw()
+ggsave(filename='writeup/2c.png', width=5, height=3)
 
 #----- END YOUR CODE BLOCK HERE -----#
 
@@ -111,6 +131,19 @@ dim(faces_matrix)
 
 #----- START YOUR CODE BLOCK HERE -----#
 
+# plot the first 9 PCs
+par(mfrow=c(3,3))
+for(i in 1:9){
+  
+  eigenfaces <- pc$rotation[, i]
+  
+  eigenfaces <- matrix(eigenfaces, 192, 168, byrow=TRUE)
+  plot(pixmapGrey(eigenfaces), main = as.character(i))
+  
+}
+dev.copy(device=png, file='writeup/2d_pca_faces.png', height=800, width=800*(168/192))
+dev.off()
+par(mfrow=c(1,1))
 
 #----- END YOUR CODE BLOCK HERE -----#
 
@@ -122,8 +155,42 @@ dim(faces_matrix)
 
 #----- START YOUR CODE BLOCK HERE -----#
 
+# read in photo
+b01 <- read.pnm(file = "datasets/CroppedYale/yaleB05/yaleB05_P00A+010E+00.pgm")
+plot(pixmapGrey(getChannels(b01)))
+
+# convert photo to vector #and center
+b01 <- as.vector(getChannels(b01), mode="any") #- as.vector(t(mean_face)
+
+# use the first n eigenfaces
+eigenfaces <- pc$rotation[, 1:120]
+
+# calculate the weights
+b01_coefs <- t(eigenfaces) %*% (b01)
+
+# create recursive list of weighted photos
+b01_comps <- lapply(1:120,
+                    function(i){
+                      
+                      if(i==1){
+                        eigenfaces[, 1] * (b01_coefs[1])
+                      } else{
+                        eigenfaces[, 1:i] %*% (b01_coefs[1:i])
+                      }
+                    }
+)
+
+
+plot(pixmapGrey(t(matrix(b01_comps[[120]], 168, 192))))
+
+
+
 
 #----- END YOUR CODE BLOCK HERE -----#
+
+
+
+
 
 
 
