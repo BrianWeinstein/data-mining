560623a89112636cdb5acaf425e1b263b56cfcea
BrianWeinstein <bweinstein02@gmail.com>
2015-07-21 01:44:29 -0400

    part of problem 5b

--- homework_2/hw02_q5_partial.R
+++ homework_2/hw02_q5_partial.R
@@ -44,22 +44,37 @@ distances <- as.matrix(distances, type="any")
 
 #----- START YOUR CODE BLOCK HERE -----#
 
-GetEstimate <- function(testPoint, k){
-  # write a function to estimate the value of a point left out in LOO cross validation in kNN
-  # Inputs: testPoint = row number of point to leave out
-  #         k = number of nearest neighbors to use
+
+GetEstimate <- function(k, pointNumber, testPoint){
+  # write a function to estimate the value of a point in LOO CV in kNN
+  # Inputs: k = number of nearest neighbors to use
+  #         pointNumber = row number of point to estimate
+  #         testPoint = row number of point to leave out
   # Output: estimated Y value for the point in row testPoint
   
-  # get the row numbers of the k nearest neighbors, excluding the point iteself
-  nearestPtRow <- which(distances[testPoint, ] %in% sort(distances[testPoint, ])[2:(k+1)])
+  # get the row numbers of the k nearest neighbors to pointNumber
+  nearestPtRows <- which(distances[pointNumber, ] %in% sort(distances[pointNumber,][-testPoint])[1:k])
   
   # estimate the left out value as the mean of the k nearest y values
-  mean(rawData[nearestPtRow, "y"])
-
+  mean(rawData$y[nearestPtRows])
+  
 }
 
 
 
+k=1
+testPoint=3
+
+# for a given k and test point, estimate the values at each point
+estimates <- as.vector(
+  sapply(1:nrow(rawData),
+         function(pointNumber){
+           estimates <- GetEstimate(k=k, pointNumber=pointNumber, testPoint=testPoint)
+         }
+  )
+)
+
+estimates
 
 
 
@@ -69,11 +84,38 @@ GetEstimate <- function(testPoint, k){
 
 #----- END YOUR CODE BLOCK HERE -----#
 
+
+
+
 #################
 # Problem 5c
 #################
 
 #----- START YOUR CODE BLOCK HERE -----#
+
+
+GetEstimate <- function(testPoint, k){
+  # write a function to estimate the value of a point left out in LOO cross validation in kNN
+  # Inputs: testPoint = row number of point to leave out
+  #         k = number of nearest neighbors to use
+  # Output: estimated Y value for the point in row testPoint
+  
+  # get the row numbers of the k nearest neighbors
+  nearestPtRows <- which(distances[testPoint, ] %in% sort(distances[testPoint, ])[1:k])
+  
+  # estimate the left out value as the mean of the k nearest y values
+  mean(rawData[nearestPtRows, "y"])
+  
+}
+
+TrainMSE <- function(true, estimate){
+  mean((true-estimate)^2)
+  
+  
+}
+
+
+
 
 
 #----- END YOUR CODE BLOCK HERE -----#
